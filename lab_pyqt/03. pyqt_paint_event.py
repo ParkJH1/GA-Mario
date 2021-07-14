838db6e7a125dfe499d7bdcf724d04cafc6107c2
박정현 <pjh9996@naver.com>
2021-07-14 20:50:21 +0900

    Update 03. pyqt_paint_event.py

--- lab_pyqt/03. pyqt_paint_event.py
+++ lab_pyqt/03. pyqt_paint_event.py
@@ -1,5 +1,5 @@
 # 03. pyqt_paint_event.py
-# PyQt Paint Event
+# PyQt 그리기
 import sys
 from PyQt5.QtGui import QPainter, QPen, QBrush, QColor
 from PyQt5.QtCore import Qt
@@ -9,8 +9,11 @@ from PyQt5.QtWidgets import QApplication, QWidget
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
+        # 창 크기 고정
         self.setFixedSize(200, 300)
-        self.setWindowTitle('GA Mario')
+        # 창 제목 설정
+        self.setWindowTitle('MyApp')
+        # 창 띄우기
         self.show()
 
     # 창이 업데이트 될 때마다 실행되는 함수
@@ -29,7 +32,7 @@ class MyApp(QWidget):
         painter.setPen(QPen(QColor.fromRgb(255, 0, 0), 3.0, Qt.SolidLine))
         # 브러쉬 설정 (채우기)
         painter.setBrush(QBrush(Qt.blue))
-        # 직사각형
+        # 직사각형 그리기
         painter.drawRect(0, 100, 100, 100)
 
         painter.setPen(QPen(Qt.black, 1.0, Qt.SolidLine))
@@ -39,15 +42,17 @@ class MyApp(QWidget):
         painter.drawEllipse(100, 100, 100, 100)
 
         painter.setPen(QPen(Qt.cyan, 1.0, Qt.SolidLine))
+        # 브러쉬 초기화
         painter.setBrush(Qt.NoBrush)
         # 텍스트 그리기
         painter.drawText(0, 250, 'abcd')
 
+        # 그리기 끝
         painter.end()
 
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   window = MyApp()
-   sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    window = MyApp()
+    sys.exit(app.exec_())
 
